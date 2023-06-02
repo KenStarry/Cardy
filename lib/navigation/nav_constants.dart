46fafc79d08499653227d91fa71ee11c69c638f7
KenStarry <starrycodes@gmail.com>
2023-06-02 12:01:43 +0300

    Added Get Pages and routes

---
+++ lib/navigation/nav_constants.dart
@@ -0,0 +1,9 @@
+class NavConstants {
+
+  /// Routes
+  static const String initialRoute = '/';
+  static const String mainRoute = '/main_screen';
+
+  /// Bottom Navigation Routes
+  static const String homeRoute = '/main_screen/home_screen';
+}
